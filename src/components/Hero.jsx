f057b0a39cfcef626c8ca3ad9d2caf44095f1748
Arjun Dev Singla <arjundevsingla1612@gmail.com>
2024-04-11 00:01:31 +0530

    Adding My First React Project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+const HeroSection = () => {
+    return (
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+                <div className="hero-button">
+                    <button>Shop Now</button>
+                    <button className="sec-btn">Category</button>
+                </div>
+                <div className="shopping">  
+                    <p>Also Available On</p>
+
+                    <div className="brand-icons">
+                        <img src="/images/flipkart.png" alt="flipkart" />
+                        <img src="/images/amazon.png" alt="amazon" />
+                    </div>
+                </div>
+            </div>
+            <div className="hero-logo">
+                <img src="/images/shoe_image.png" alt="Shoe Image" />
+            </div>
+
+        </main>
+    )
+}
+
+export default HeroSection;
